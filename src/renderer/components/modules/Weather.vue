a132146ea432efdc7c7fc07a0511c23145ee249b
wh1tecat <whitequeen0@gmail.com>
2018-07-13 10:58:05 +0900

    [WIP] create Weather

---
+++ src/renderer/components/modules/Weather.vue
@@ -0,0 +1,31 @@
+<template>
+	<div id="weather_module">
+
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Weather",
+		mounted() {
+
+		},
+		data () {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style scoped>
+	span {
+		display: block;
+		float: right;
+		width: 80%;
+		font-size: 4vw;
+	}
+</style>
